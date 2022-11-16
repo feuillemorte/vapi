dccabf47eec7fd7f7811c4fe19ce9145c253b305
0xfm <it@fmorte.org>
2022-11-16 13:52:31 +0100

    Changed the method of running the API

--- src/dull_api.py
+++ src/dull_api.py
@@ -1,6 +1,5 @@
 """Small vulnerable API. WARNING: DO NOT USE THIS API IN PRODUCTION!"""
 import logging
-from logging.handlers import RotatingFileHandler
 
 from flask import Flask
 from flask_smorest import Api
@@ -8,17 +7,10 @@ from flask_smorest import Api
 from . import db
 from .blueprints import pets
 
-
-handler = RotatingFileHandler("app.log", maxBytes=1000000, backupCount=5)
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
 db.init()
 app = Flask(__name__)
-
-handler.setLevel(logging.DEBUG)
-app.logger.addHandler(handler)
-log = logging.getLogger("werkzeug")
-log.setLevel(logging.DEBUG)
-log.addHandler(handler)
 
 app.config["API_TITLE"] = "Small vulnerable API (DO NOT USE IN PRODUCTION)"
 app.config["API_VERSION"] = "v1"
--- start.sh
+++ start.sh
@@ -5,24 +5,26 @@ fi
 
 TIMEOUT=10
 export FLASK_APP=dull_api
-flask run --host=0.0.0.0 --port 5000 >app.log 2>&1 &
+nohup flask run --host=0.0.0.0 --port 5000 2> error.log > /dev/null &
+echo $!
+
 
 COUNTER=0
 while true ; do
     if [ $COUNTER -gt $TIMEOUT ]; then
         >&2 echo "Can't run flask. Please, check the app.log for details:"
-        if [ -f app.log ]; then
-            >&2 cat app.log
+        echo ""
+        if [ -f error.log ]; then
+            >&2 cat error.log
         fi
         exit 1
     fi
     if [ -f app.log ]; then
         if grep -q -i "running on" app.log; then
+            grep -i "running on " app.log | awk '{print $4}'
             break
         fi
     fi
     sleep 1
     COUNTER=$((COUNTER+1))
 done
-
-echo $!
